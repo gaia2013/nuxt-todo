bfdd539d1a660057636ffc0afaa295a12470cd3c
gaia_galaxy <hitoshi.gaia.galaxy@gmail.com>
2021-12-09 12:17:38 +0900

    implement index.js

---
+++ store/index.js
@@ -0,0 +1,9 @@
+//  vuexfire mutations の機能を読み込み
+import { vuexfireMutations } from 'vuexfire'
+
+// mutations を定義
+export const mutations = {
+  ...vuexfireMutations
+}
+
+// store/index.js に定義する必要がある
